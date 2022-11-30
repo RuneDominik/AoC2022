5f188318d70f5832fbcdb594e6e603d21692721a
Rune Michael Dominik <rune.dominik@tu-dortmund.de>
2022-11-30 15:53:29 +0100

    Add helloworld module for day0

---
+++ src/helloworld/io.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader, Error},
+    path::Path,
+};
+
+pub fn get_msg(path: impl AsRef<Path>) -> Result<String, Error> {
+    let file = File::open(path)?;
+
+    let br = BufReader::new(file);
+
+    let msg: String = br
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+
+
+    return Ok(msg);
+}
