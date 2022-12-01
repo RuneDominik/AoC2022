028ad5af10078ab32d64f0ef74f235beb110add7
Rune Michael Dominik <rune.dominik@tu-dortmund.de>
2022-12-01 12:43:28 +0100

    Add tests for day1

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod helloworld;
+mod elf;
 
 #[cfg(test)]
 mod helloworld_tests {
@@ -9,5 +10,21 @@ mod helloworld_tests {
         let iostream = io::get_msg("data/day0_data/test_data.txt").unwrap();
 
         assert_eq!(iostream, "Hello World!");
+    }
+}
+
+#[cfg(test)]
+mod elf_tests {
+    #[test]
+    fn test_calories() {
+        use crate::elf::calories;
+
+        let max_calories = calories::get_max_calories("data/day1_data/test_data.txt").unwrap();
+
+        assert_eq!(max_calories, 24000);
+
+        let top3_calories = calories::get_top3_calories("data/day1_data/test_data.txt").unwrap();
+
+        assert_eq!(top3_calories, 45000);
     }
 }
